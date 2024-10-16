d603db7d6fd140472c18bf0613c6104babd6223d
aloeXgoddammit <grishinoy2008@gmail.com>
2024-10-16 16:31:17 +0000

    лекция 3 номер 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+gravity_constant = 6.6743 * 10 ** -11
+speed_of_light = 3 * 10 ** 8
+plank_constant = 6.62607015 * 10 ** -34
+boltzmann_constant = 1.380649 * 10 ** -23
+avogadro_constant = 6.02214076 * 10 ** 23
+faraday_constant = 96485.3321233100184
+molar_Planck_constant = 3.9903127128934314 * 10 ** -10
+elementary_charge = 1.602176634 * 10 ** -19
+speed_of_light_in_vacuum = 299792458
+molar_gas_constant = 8.31446261815324
